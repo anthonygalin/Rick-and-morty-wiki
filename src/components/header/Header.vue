e57eb1ccd31534b669b9cca61a241819c213ffee
anthony Rodriguez <anthonygalin@gmail.com>
2022-03-28 02:32:34 -0400

    Main proyect

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,28 +1,27 @@
 <template>
-  <section
-    id="Header"
-    class="flex justify-between py-4 px-12 bg-black bg-opacity-10"
-  >
-    <div class="flex">
-      <img
-        src="../../assets/utils/Rick_and_Morty_Title.svg"
-        alt="headerTitle"
-        class="w-1/6"
-      />
-      <span class="text-lg">Wiki</span>
-    </div>
-    <div class="flex gap-2">
-      <div
-        v-for="link in headerLinks"
-        :key="`Link-${link.title}`"
-        class="grid content-center"
-      >
-        <router-link
-          :to="link.link"
-          exact-active-class="border-b-primary border-b-4 text-secondary rounded"
+  <section id="Header" class="py-4 bg-black bg-opacity-10">
+    <div class="flex justify-between mx-80">
+      <div class="flex">
+        <img
+          src="../../assets/utils/Rick_and_Morty_Title.svg"
+          alt="headerTitle"
+          class="w-1/6"
+        />
+        <span class="text-lg">Wiki</span>
+      </div>
+      <div class="flex gap-2">
+        <div
+          v-for="link in headerLinks"
+          :key="`Link-${link.title}`"
+          class="grid content-center"
         >
-          {{ link.title }}
-        </router-link>
+          <router-link
+            :to="link.link"
+            exact-active-class="border-b-primary border-b-4 text-secondary rounded"
+          >
+            {{ link.title }}
+          </router-link>
+        </div>
       </div>
     </div>
   </section>
