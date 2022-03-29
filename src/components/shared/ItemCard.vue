cdd855bbdab83b030f113b160906ead1dbe5d803
anthony Rodriguez <anthonygalin@gmail.com>
2022-03-28 20:22:23 -0400

    Created character view card component Optimized for pc and mobile

---
+++ src/components/shared/ItemCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col hover:transform">
+    <img
+      class="rounded-full w-auto"
+      :src="item.image"
+      :alt="`characterImg:${item.name}`"
+    />
+    <p
+      class="text-sm truncate text-center mt-2 px-2 border-2 border-primary rounded bg-black/50"
+    >
+      {{ item.name }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemCard",
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
