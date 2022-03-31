f4394ae25298100d01c84eaf52361d4a649b9550
anthonygalin <80184532+anthonygalin@users.noreply.github.com>
2022-03-31 16:02:16 -0400

    Update Header.vue

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -136,12 +136,10 @@ export default {
     },
     onSearchItem() {
       if ("Characters" === this.$route.name) {
-        this.$store.state.currentCharacter = "";
         this.$store.state.currentCharacter = this.searchItem;
         this.$store.dispatch("searchCharacter");
         this.$store.dispatch("getCharacters");
       }
-      this.$store.state.currentLocation = "";
       this.$store.state.currentLocation = this.searchItem;
       this.$store.dispatch("searchLocation");
       this.$store.dispatch("getLocations");
